8c4560b50f21c909b56ae0de05b952a971edf7d8
Irio Irineu Musskopf Junior <irio.musskopf@caixadeideias.com.br>
2012-10-16 21:51:57 -0300

    Add test for CreditCardPayment's json format

--- test/test_credit_card_payment.rb
+++ test/test_credit_card_payment.rb
@@ -26,4 +26,23 @@ class TestCreditCardPayment < Test::Unit::TestCase
     subject = MyMoip::CreditCardPayment.new(credit_card)
     assert_equal 1, subject.tranches
   end
+
+  def test_json_format
+    payment = MyMoip::CreditCardPayment.new(Fixture.credit_card)
+    assert_equal "CartaoCredito", payment.to_json[:Forma]
+    assert payment.to_json[:Parcelas].kind_of?(Integer), "'Parcelas' must be a kind of integer."
+  end
+
+  def test_json_credit_card_format
+    payment = MyMoip::CreditCardPayment.new(Fixture.credit_card)
+    assert_match /\A\d+\z/, payment.to_json[:CartaoCredito][:Numero]
+    assert_match /\A((0[1-9])|(1[02]))\/\d{2}\z/, payment.to_json[:CartaoCredito][:Expiracao]
+    assert_match /\A\d{3}\z/, payment.to_json[:CartaoCredito][:CodigoSeguranca]
+    original_date = Date.new(1980, 11, 3)
+    MyMoip::CreditCard.any_instance.stubs(:owner_birthday).returns(original_date)
+    assert_equal original_date.strftime("%d/%m/%Y"), payment.to_json[:CartaoCredito][:Portador][:DataNascimento]
+    assert_match /\A\(\d{2}\)\d{4,5}-\d{4}/, payment.to_json[:CartaoCredito][:Portador][:Telefone]
+    assert_match /\A\d+\z/, payment.to_json[:CartaoCredito][:Portador][:Identidade]
+  end
+
 end
