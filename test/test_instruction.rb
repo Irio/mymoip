4d407b72ec5a63f28a44aea2585a993cb66d5290
Irio Irineu Musskopf Junior <irio.musskopf@caixadeideias.com.br>
2012-10-16 22:47:20 -0300

    Add test for Instruction's xml version

--- test/test_instruction.rb
+++ test/test_instruction.rb
@@ -25,4 +25,13 @@ class TestInstruction < Test::Unit::TestCase
     assert_equal String, instruction.to_xml.class
   end
 
+  def test_xml_format
+    payer       = Fixture.payer
+    instruction = Fixture.instruction(payer)
+    expected_format = <<XML
+<EnviarInstrucao><InstrucaoUnica TipoValidacao=\"Transparente\"><Razao>some payment_reason</Razao><Valores><Valor moeda=\"BRL\">100.00</Valor><Valor moeda=\"BRL\">200.00</Valor></Valores><IdProprio>some id</IdProprio><Pagador><Nome>some name</Nome><Email>some email</Email><IdPagador>some id</IdPagador><EnderecoCobranca><Logradouro>some address_street</Logradouro><Numero>some address_street_number</Numero><Complemento>some address_street_extra</Complemento><Bairro>some address_neighbourhood</Bairro><Cidade>some address_city</Cidade><Estado>some address_state</Estado><Pais>some address_country</Pais><CEP>some address_cep</CEP><TelefoneFixo>some address_phone</TelefoneFixo></EnderecoCobranca></Pagador></InstrucaoUnica></EnviarInstrucao>
+XML
+    assert_equal expected_format.rstrip, instruction.to_xml
+  end
+
 end
