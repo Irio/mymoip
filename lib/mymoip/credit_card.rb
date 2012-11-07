185c6a5891c2642da33f85d552863ae98f8d99aa
Irio Irineu Musskopf Junior <irio.musskopf@caixadeideias.com.br>
2012-11-06 23:43:50 -0200

    Add validations to CreditCard required fields by Moip
    * Accept owner_phone with dashes or parenthesis
    * Accept owner_birthday as a string
    * Accept owner_rg with dashes or dots

--- lib/mymoip/credit_card.rb
+++ lib/mymoip/credit_card.rb
@@ -1,11 +1,16 @@
 module MyMoip
   class CreditCard
+    include ActiveModel::Validations
+
     attr_accessor :logo, :card_number, :expiration_date, :security_code,
                 :owner_name, :owner_birthday, :owner_phone, :owner_rg
 
     AVAILABLE_LOGOS = [
       :american_express, :diners, :hipercard, :mastercard, :visa
     ]
+
+    validates_presence_of :logo, :security_code
+    validates_length_of :owner_phone, within: 10..11
 
     def initialize(params)
       @logo            = params[:logo]            if params.has_key? :logo
@@ -16,6 +21,31 @@ module MyMoip
       @owner_birthday  = params[:owner_birthday]  if params.has_key? :owner_birthday
       @owner_phone     = params[:owner_phone]     if params.has_key? :owner_phone
       @owner_rg        = params[:owner_rg]        if params.has_key? :owner_rg
+    end
+
+    def owner_birthday=(value)
+      unless value.nil?
+        value = Date.parse(value.to_s)
+      end
+      @owner_birthday = value
+    end
+
+    def owner_phone=(value)
+      unless value.nil?
+        # Removes non-digits
+        value.gsub!(/\D*/, '')
+        # Removes zeros in the beginning
+        value.gsub!(/\A0*/, '')
+      end
+      @owner_phone = value
+    end
+
+    def owner_rg=(value)
+      unless value.nil?
+        # Removes dashes and dots
+        value.gsub!(/\-|\./, '')
+      end
+      @owner_rg = value
     end
   end
 end
--- test/test_creditcard.rb
+++ test/test_creditcard.rb
@@ -1,7 +1,6 @@
 require 'helper'
 
 class TestCreditCard < Test::Unit::TestCase
-
   def test_initialization_and_setters
     subject = MyMoip::CreditCard.new(
       logo: :visa,
@@ -24,4 +23,68 @@ class TestCreditCard < Test::Unit::TestCase
     assert_equal "1010202030", subject.owner_rg
   end
 
+  def test_validate_presence_of_logo_attribute
+    subject = Fixture.credit_card
+    subject.logo = nil
+    assert subject.invalid? && subject.errors[:logo].present?,
+      'should be invalid without a logo'
+  end
+
+  def test_owner_birthday_accepts_string_version_of_dates
+    subject = Fixture.credit_card
+    subject.owner_birthday = '20/12/1980'
+    assert_equal Date.new(1980, 12, 20), subject.owner_birthday
+  end
+
+  def test_validate_presence_of_security_code_attribute
+    subject = Fixture.credit_card
+    subject.security_code = nil
+    assert subject.invalid? && subject.errors[:security_code].present?,
+      'should be invalid without an security_code'
+  end
+
+  def test_validate_length_of_owner_phone_attribute_in_10_or_11_chars
+    subject = Fixture.credit_card
+    subject.owner_phone = '5130405060'
+    assert subject.valid?, 'should accept 10 chars'
+    subject.owner_phone = '51930405060'
+    assert subject.valid?, 'should accept 11 chars'
+    subject.owner_phone = '215130405060'
+    assert subject.invalid? && subject.errors[:owner_phone].present?,
+      'should not accept strings with other than 10 or 11 chars'
+  end
+
+  def test_remove_left_zeros_from_owner_phone
+    subject = Fixture.credit_card
+    subject.owner_phone = '05130405060'
+    assert_equal '5130405060', subject.owner_phone
+  end
+
+  def test_remove_dashes_from_owner_phone
+    subject = Fixture.credit_card
+    subject.owner_phone = '513040-5060'
+    assert_equal '5130405060', subject.owner_phone
+    subject.owner_phone = '5193040-5060'
+    assert_equal '51930405060', subject.owner_phone
+  end
+
+  def test_remove_parenthesis_from_owner_phone
+    subject = Fixture.credit_card
+    subject.owner_phone = '(51)30405060'
+    assert_equal '5130405060', subject.owner_phone
+    subject.owner_phone = '(51)930405060'
+    assert_equal '51930405060', subject.owner_phone
+  end
+
+  def test_remove_dashes_from_owner_rg
+    subject = Fixture.credit_card
+    subject.owner_rg = '91122534-1'
+    assert_equal '911225341', subject.owner_rg
+  end
+
+  def test_remove_dots_from_owner_rg
+    subject = Fixture.credit_card
+    subject.owner_rg = '91.122.5341'
+    assert_equal '911225341', subject.owner_rg
+  end
 end
