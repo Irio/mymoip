69b3fd28b3aa08de4656eb11c1d564756234fb3e
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2013-08-28 22:06:53 -0300

    Remove unnecessary block in url validator

--- lib/mymoip/validators.rb
+++ lib/mymoip/validators.rb
@@ -3,12 +3,10 @@ module MyMoip
     private
 
     def valid_url?(url)
-      begin
-        uri = URI.parse(url)
-        uri.kind_of?(URI::HTTP) or uri.kind_of?(URI::HTTPS)
-      rescue URI::InvalidURIError
-        false
-      end
+      uri = URI.parse(url)
+      uri.kind_of?(URI::HTTP) or uri.kind_of?(URI::HTTPS)
+    rescue URI::InvalidURIError
+      false
     end
   end
 end
