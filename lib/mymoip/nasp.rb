e82bf3623723716632a26995bd4056e8cee72d82
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2013-09-30 10:20:07 -0300

    Add NASP support

---
+++ lib/mymoip/nasp.rb
@@ -0,0 +1,60 @@
+module MyMoip
+  class Nasp
+    STATUSES = {
+      1 => "Autorizado",
+      2 => "Iniciado",
+      3 => "BoletoImpresso",
+      4 => "Concluido",
+      5 => "Cancelado",
+      6 => "EmAnalise",
+      7 => "Estornado",
+      9 => "Reembolsado"
+    }
+
+    attr_accessor :id, :value, :status, :moip_code, :payment_mode,
+                  :payment_method, :installments, :payer_email,
+                  :credit_card_first_digits, :credit_card_last_digits,
+                  :credit_card_flag, :moip_vault
+
+    def initialize(attrs)
+      attrs.each do |attr, value|
+        public_send(:"#{attr_map(attr)}=", value)
+      end
+    end
+
+    def done?
+      status == 4
+    end
+
+    def canceled?
+      status == 5
+    end
+
+    def reversed?
+      status == 7
+    end
+
+    def refunded?
+      status == 9
+    end
+
+    private
+
+    def attr_map(attr)
+      {
+        id_transacao: :id,
+        valor: :value,
+        status_pagamento: :status,
+        cod_moip: :moip_code,
+        forma_pagamento: :payment_mode,
+        tipo_pagamento: :payment_method,
+        parcelas: :installments,
+        email_consumidor: :payer_email,
+        cartao_bin: :credit_card_first_digits,
+        cartao_final: :credit_card_last_digits,
+        cartao_bandeira: :credit_card_flag,
+        cofre: :moip_vault
+      }.fetch(attr.to_sym)
+    end
+  end
+end
---
+++ test/lib/test_nasp.rb
@@ -0,0 +1,95 @@
+require_relative "../test_helper"
+
+class TestPayer < Test::Unit::TestCase
+  def test_getters_for_attributes
+    nasp = MyMoip::Nasp.new(
+      id_transacao: "some id",
+      valor: 2350,
+      status_pagamento: 4,
+      cod_moip: 3000,
+      forma_pagamento: 7,
+      tipo_pagamento: "CartaoDeCredito",
+      parcelas: 1,
+      email_consumidor: "joe@mail.com",
+      cartao_bin: "123456",
+      cartao_final: "4321",
+      cartao_bandeira: "AmericanExpress",
+      cofre: "4780c1fb-e47d-448e-ad7b-506c125366fc"
+    )
+
+    assert_equal "some id", nasp.id
+    assert_equal 2350, nasp.value
+    assert_equal 4, nasp.status
+    assert_equal 3000, nasp.moip_code
+    assert_equal 7, nasp.payment_mode
+    assert_equal "CartaoDeCredito", nasp.payment_method
+    assert_equal 1, nasp.installments
+    assert_equal "joe@mail.com", nasp.payer_email
+    assert_equal "123456", nasp.credit_card_first_digits
+    assert_equal "4321", nasp.credit_card_last_digits
+    assert_equal "AmericanExpress", nasp.credit_card_flag
+    assert_equal "4780c1fb-e47d-448e-ad7b-506c125366fc", nasp.moip_vault
+  end
+
+  def test_initialization_and_setters_with_string_keys
+    nasp = MyMoip::Nasp.new(
+      "id_transacao" => "some id",
+      "valor" => 2350,
+      "status_pagamento" => 4,
+      "cod_moip" => 3000,
+      "forma_pagamento" => 7,
+      "tipo_pagamento" => "CartaoDeCredito",
+      "parcelas" => 1,
+      "email_consumidor" => "joe@mail.com",
+      "cartao_bin" => "123456",
+      "cartao_final" => "4321",
+      "cartao_bandeira" => "AmericanExpress",
+      "cofre" => "4780c1fb-e47d-448e-ad7b-506c125366fc"
+    )
+
+    assert_equal "some id", nasp.id
+    assert_equal 2350, nasp.value
+    assert_equal 4, nasp.status
+    assert_equal 3000, nasp.moip_code
+    assert_equal 7, nasp.payment_mode
+    assert_equal "CartaoDeCredito", nasp.payment_method
+    assert_equal 1, nasp.installments
+    assert_equal "joe@mail.com", nasp.payer_email
+    assert_equal "123456", nasp.credit_card_first_digits
+    assert_equal "4321", nasp.credit_card_last_digits
+    assert_equal "AmericanExpress", nasp.credit_card_flag
+    assert_equal "4780c1fb-e47d-448e-ad7b-506c125366fc", nasp.moip_vault
+  end
+
+  def test_if_status_is_done
+    nasp = Fixture.nasp(status_pagamento: 4)
+    assert_equal nasp.done?, true
+
+    nasp = Fixture.nasp(status_pagamento: 3)
+    assert_equal nasp.done?, false
+  end
+
+  def test_if_status_is_canceled
+    nasp = Fixture.nasp(status_pagamento: 5)
+    assert_equal nasp.canceled?, true
+
+    nasp = Fixture.nasp(status_pagamento: 3)
+    assert_equal nasp.canceled?, false
+  end
+
+  def test_if_status_is_reversed
+    nasp = Fixture.nasp(status_pagamento: 7)
+    assert_equal nasp.reversed?, true
+
+    nasp = Fixture.nasp(status_pagamento: 3)
+    assert_equal nasp.reversed?, false
+  end
+
+  def test_if_status_is_refunded
+    nasp = Fixture.nasp(status_pagamento: 9)
+    assert_equal nasp.refunded?, true
+
+    nasp = Fixture.nasp(status_pagamento: 3)
+    assert_equal nasp.refunded?, false
+  end
+end
