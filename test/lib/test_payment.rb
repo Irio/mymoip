b378878a618fc042aa64038878715285ef419cba
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2013-10-12 15:41:18 -0300

    ajust test_payment assets arguments order

--- test/lib/test_payment.rb
+++ test/lib/test_payment.rb
@@ -2,14 +2,14 @@ require_relative '../test_helper'
 
 class TestPayment < Test::Unit::TestCase
   def test_payment_method_when_payment_split
-    assert_equal MyMoip::PaymentSlipPayment.payment_method, :payment_slip
+    assert_equal :payment_slip, MyMoip::PaymentSlipPayment.payment_method
   end
 
   def test_payment_method_when_bank_debit
-    assert_equal MyMoip::BankDebitPayment.payment_method, :bank_debit
+    assert_equal :bank_debit, MyMoip::BankDebitPayment.payment_method
   end
 
   def test_payment_method_when_credit_card
-    assert_equal MyMoip::CreditCardPayment.payment_method, :credit_card
+    assert_equal :credit_card, MyMoip::CreditCardPayment.payment_method
   end
 end
