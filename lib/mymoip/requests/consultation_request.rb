55458611891e5cb4834d81d2d6d1dba55dce5f25
Oswaldo S Ferreira <oswaldo.ferreira@iff.edu.br>
2014-07-25 10:43:47 -0300

    payment transactions finding on consultation

--- lib/mymoip/requests/consultation_request.rb
+++ lib/mymoip/requests/consultation_request.rb
@@ -1,3 +1,5 @@
+require 'active_support/core_ext/hash'
+
 module MyMoip
   class ConsultationRequest < Request
 
@@ -15,9 +17,31 @@ module MyMoip
       super(params, opts)
     end
 
-    def xml_str
-      @response.body
+    def response_hash
+      Hash.from_xml(@response.body)
     end
 
+    def transactions
+      @transactions ||= get_payment_nodes
+    end
+
+    def transaction(moip_code)
+      transactions.select { |i| i.key(format_moip_code(moip_code)) }.first
+    end
+
+    private
+
+    def format_moip_code(moip_code)
+      moip_code.to_s.rjust(12, '0').scan(/\d{4}/).join('.')
+    end
+
+    def get_payment_nodes
+      payments = response_hash['ConsultarTokenResponse']\
+                              ['RespostaConsultar']\
+                              ['Autorizacao']\
+                              ['Pagamento']
+
+      payments.is_a?(Hash) ? [payments] : payments
+    end
   end
 end
--- test/lib/test_consultation_request.rb
+++ test/lib/test_consultation_request.rb
@@ -13,11 +13,55 @@ class TestConsultationRequest < Test::Unit::TestCase
     assert_equal true, MyMoip::ConsultationRequest::REQUIRES_AUTH
   end
 
-  def test_should_provide_the_xml_str_get_by_the_request
+  def test_should_provide_the_response_as_a_hash
     request = MyMoip::ConsultationRequest.new('U260F1E2P0G4N0Z2T1S0M4T3C5E4J5M8L5U0G0I0U0M0H0Y0E3X7S9X6I783')
 
-    VCR.use_cassette('consultation_request') { request.api_call }
+    VCR.use_cassette('consultation_with_mult_transactions_request') {
+      request.api_call
+    }
 
-    assert_block { request.xml_str.match(/^<ns1:ConsultarTokenResponse/) }
+    response_hash = request.response_hash
+    assert_equal Hash, response_hash.class
+    assert response_hash.keys.include?('ConsultarTokenResponse')
+  end
+
+  def test_transactions_when_response_has_multiple_transactions
+    request = MyMoip::ConsultationRequest.new('U260F1E2P0G4N0Z2T1S0M4T3C5E4J5M8L5U0G0I0U0M0H0Y0E3X7S9X6I783')
+
+    VCR.use_cassette('consultation_with_mult_transactions_request') {
+      request.api_call
+    }
+
+    assert_equal 5, request.transactions.length
+    assert_equal Array, request.transactions.class
+    assert_equal false, request.transactions.empty?
+  end
+
+  def test_transactions_when_response_has_one_transaction
+    request = MyMoip::ConsultationRequest.new('ASDFF1E2P0G4N0Z2T1S0M4T3C5E4J5M8L5U0G0I0U0M0H0Y0E3X759X6ASDF')
+
+    VCR.use_cassette('consultation_with_one_transaction_request') {
+      request.api_call
+    }
+
+    assert_equal 1, request.transactions.length
+    assert_equal Array, request.transactions.class
+    assert_equal false, request.transactions.empty?
+  end
+
+  def test_find_a_transaction_by_formatted_moip_code_on_transactions_list
+    request = MyMoip::ConsultationRequest.new('TOKEN')
+
+    request.stubs(:transactions).returns(
+      [{ moip_code: '0000.2524.0547' }, { moip_code: '0000.1234.0547'}]
+    )
+
+    transaction = request.transaction('12340547')
+    assert_equal({ moip_code: '0000.1234.0547'}, transaction)
+  end
+
+  def test_private_format_moip_code
+    request = MyMoip::ConsultationRequest.new('TOKEN')
+    assert_equal '0000.2524.0547', request.send(:format_moip_code, '25240547')
   end
 end
